f4c6e5cd47be5a937cee6300ee93dab0f6946c0c
marysdad <justonly4testing@gmail.com>
2022-09-14 12:10:08 +0300

    bringing to the ETL pipeline into one file.
    adding file variables and data connections

--- final_ETL.py
+++ final_ETL.py
@@ -10,3 +10,13 @@ load_file = 'mysql_export.csv'
 load_file = os.path.join(current_path, load_file)
 
 
+proj='bigquery-demo-361613'
+dataset = 'dataset1'
+target_table = 'annual_movie_summary'
+table_id = f'{proj}.{dataset}.{target_table}'
+
+
+# data connections
+connection = mysql.connector.connect(read_default_file='/home/kaki/.my.cnf')
+client = bigquery.Client(project=proj)
+
